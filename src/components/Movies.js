b75f7b6fb2b9aaa8ffb26e584485e4301014167a
louie_gomez <kinggomez027.lg@gmail.com>
2022-09-05 17:39:02 -0400

    movies list

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,12 +2,21 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
+  const movieItems = movies.map((movie) => (
+    <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <p>Time: {movie.time} min.</p>
+      <ul>
+        {movie.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ))
   return (
     <div>
       <h1>Movies Page</h1>
-      <div>
-        
-      </div>
+      {movieItems}
     </div> 
   );
 }
