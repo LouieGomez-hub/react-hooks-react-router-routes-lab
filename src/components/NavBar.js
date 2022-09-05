9d16924af9086fe17ba0a2497e0345c922427ddd
louie_gomez <kinggomez027.lg@gmail.com>
2022-09-05 17:24:51 -0400

    render navlinks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,46 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavLink
+        to="/"
+        exact
+        activeStyle={{
+          background: "grey",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/movies"
+        exact
+        activeStyle={{
+          background: "grey",
+        }}
+      >
+        Movies
+      </NavLink>
+      <NavLink
+        to="/directors"
+        exact
+        activeStyle={{
+          background: "grey",
+        }}
+      >
+        Directors
+      </NavLink>
+      <NavLink
+        to="/actors"
+        exact
+        activeStyle={{
+          background: "grey",
+        }}
+      >
+        Actors
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
